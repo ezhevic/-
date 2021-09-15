93e9918dc53a93560c58a49b5aae53afc39a9837
ezhevic <90516987+ezhevic@users.noreply.github.com>
2021-09-15 22:11:16 +0300

    Add files via upload

--- Задание 1/src/main/java/Task1.java
+++ Задание 1/src/main/java/Task1.java
@@ -4,21 +4,38 @@ public class Task1
     //0.5 балла - если посчитаете в цикле
     //1 балл - если посчитаете рекурсией
     public static int fact(int n){
-        return n;
+        int f = 1;
+        if (n == 0 || n == 1){
+            return f;
+        }
+        else{
+            f = f * n * fact(n - 1);
+            return f;
+        }
     }
 
     //вывести таблицу умножения на экран - 1 балл
     //подсказка - использовать двойной for
     public static void table(){
-        //Ваше решение здесь
+       for (int i = 1; i <= 10; i++) {
+           for (int j = 1; j <= 10; j++) {
+               System.out.print (i*j + "  ");
+           }
+           System.out.println("");
+       }
     }
 
     //посчитать сумму цифр числа
     //можно посчитать для трехзначного - 0.5 балла
     //для любого числа - 1 балл
     //подсказка - в случае для любого числа используйте while
-    public static int sum(int n){
-        //Ваше решение здесь
+    public static int sum(int n) {
+        int nom = n;
+        n = 0;
+        while (nom > 0) {
+            n += (nom % 10);
+            nom /= 10;
+        }
         return n;
     }
 
@@ -29,9 +46,11 @@ public class Task1
     //Годы 1600 и 2000 - високосные, так как они кратны 100 и кратны 400.
     //Годы 2100, 2200 и 2300 - не високосные.
     //за правильный ответ - 0.5 балла
-    public static boolean isLeapYear(int year) {
-        //Ваше решение здесь
-        return true;
+   public static boolean isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            return true;
+        }
+        return false;
     }
 
     //здесь вам нужно будет использовать результат прошлой задачи
@@ -39,26 +58,54 @@ public class Task1
     //правильный ответ - 0.5 балла
     public static int daysInYear(int year) {
         if (isLeapYear(year)){
-            //
-        } else {
-            //
+            return 366;
         }
-        return 0;
+        else {
+            return 365;
+        }
+        //return 0;
     }
 
     //определить номер дня недели по строке
     //например: Понедельник - 1
     //правильный ответ - 1 балл
     public static int dayOfTheWeek(String n){
-        //Ваше решение здесь
-        return 0;
+        if (n == "Понедельник") {
+            return 1;
+        }
+        if (n == "Вторник") {
+            return 2;
+        }
+        if (n == "Среда") {
+            return 3;
+        }
+        if (n == "Четверг") {
+            return 4;
+        }
+        if (n == "Пятница") {
+            return 5;
+        }
+        if (n == "Суббота") {
+            return 6;
+        }
+        if (n == "Воскресенье") {
+            return 7;
+        }
+       return 0;
     }
 
     //вывести массив на экран в виде: [1, 5, 3, 7, 10, 2, 5]
     //правильное решение - 0.5 балла
-    public static void printArray(int[] array){
-        //Ваше решение здесь
+    public static void printArray(int[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.print(array[array.length - 1] + "]");
+        System.out.println();
     }
+
+
 
     //вывести двойной массив на экран в виде:
     // [1, 5, 3, 7, 10, 2, 5]
@@ -66,7 +113,13 @@ public class Task1
     // ...
     //правильное решение - 0.5 балла
     public static void printArray(int[][] array){
-        //Ваше решение здесь
+        for (int j = 0; j < 2; j++) {
+            System.out.print("[");
+            for (int i = 0; i < 7; i++) {
+                System.out.print(" " + array[j][i] + ", ");
+            }
+        System.out.println ("]");
+        }
     }
 
     //отсортировать одномерный массив в порядке возрастания
@@ -74,7 +127,16 @@ public class Task1
     //метод пузырька (один из самых простых для понимания)
     //правильный ответ - 1 балл
     public static int[] sort(int[] array){
-        //Ваше решение здесь
+        int a = 0;
+            for (int i = 0; i < array.length; ++i){
+                for (int j = 0; j < i; ++j){
+                    if (array [i] < array [j]){
+                        a = array [j];
+                        array[j] = array [i];
+                        array [i] = a;
+                    }
+                }
+            }
         return array;
     }
 
