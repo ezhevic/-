3b6ab2f1442f9d6d4e0fce4f7d251ff844445b4c
ezhevic <90516987+ezhevic@users.noreply.github.com>
2021-11-04 22:05:11 +0300

    hw5

---
+++ src/SingletonTask.java
@@ -0,0 +1,32 @@
+public final class SingletonTask
+{
+    private static SingletonTask instance;
+    String value;
+
+    private SingletonTask(String value)
+    {
+        // Этот код эмулирует медленную инициализацию
+        try
+        {
+            Thread.sleep(10000);
+        }
+        catch (InterruptedException ex)
+        {
+            ex.printStackTrace();
+        }
+        this.value = value;
+    }
+
+    public static SingletonTask getInstance(String value)
+    {
+        if (instance == null)
+        {
+            synchronized (SingletonTask.class) {
+                if (instance == null) {
+                    instance = new SingletonTask(value);
+                }
+            }
+        }
+        return instance;
+    }
+}
